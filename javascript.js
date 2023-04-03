ffc1f542fc59e2773bc61f6b675b4c120ffb4e7e
ledo9124 <leminhdo904@gmail.com>
2023-04-03 12:59:01 +0700

    1

---
+++ javascript.js
@@ -0,0 +1,32 @@
+
+
+
+
+const bookStore = {
+    listBooks: [
+        {
+            name: '',
+            image: '',
+            description: '',
+            price: ''
+        }
+    ],
+
+    listBanners : [
+        ''
+    ],
+
+
+    handleEvents : function() {
+        //Xử lý line khi active vô navigation
+        
+    },
+
+    start : function(){
+
+
+        this.handleEvents();
+    }
+}
+
+bookStore.start();
