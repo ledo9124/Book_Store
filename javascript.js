24e63f2ff039cea3c7436590d35849456c6ff30e
ledo9124 <leminhdo904@gmail.com>
2023-04-05 12:32:06 +0700

    finish slideshow

--- javascript.js
+++ javascript.js
@@ -1,10 +1,9 @@
 const ulElement = document.querySelectorAll('li a');
 const ulActiveElement = document.querySelector('li a.active');
 const line = document.querySelector('.line');
-const infoSlide = document.querySelector('.info');
+const infoSlide = document.querySelector('.box-text');
 const slider = document.querySelector('.slider');
 const btnSlide = document.querySelector('.btn-slide');
-
 
 const bookStore = {
     listBooks: [
@@ -18,19 +17,16 @@ const bookStore = {
 
     listBanners : [
         {
-            id : 1,
             image: './asset/img/doramon3.jpg',
             title: 'Full Bộ Truyện Doramon',
             description: 'Nội dung series kể về cuộc đời bất hạnh của cậu bé Nobita và chú mèo máy Doraemon từ tương lai đến để giúp cuộc sống của cậu bé trở nên tốt hơn.'
         },
         {
-            id : 4,
             image: './asset/img/sach2.jpg',
             title: 'Truyện Cổ Tích Việt Nam',
             description: 'Văn học Việt Nam Truyện cổ tích Việt Nam là những truyện cổ tích được người Việt truyền miệng trong dân gian để kể lại những câu chuyện tưởng tượng xoay quanh một số nhân vật và sự kiện khác nhau.'
         },
         {
-            id : 3,
             image: './asset/img/tgdq1.jpg',
             title: 'Thanh Gươm Diệt Quỷ - Kimetsu No Yaiba',
             description: 'Bộ Sách Thanh Gươm Diệt Quỷ - Kimetsu No Yaiba (Trọn Bộ 23 Tập) Vào thời Taisho, có một cậu bé bán than với tấm lòng nhân hậu tên Tanjiro. Những ngày yên bình đã chẳng còn khi Quỷ đến tàn sát cả gia đình cậu, chỉ duy nhất người em gái Nezuko còn sống sót nhưng lại bị biến thành Quỷ.'
@@ -42,32 +38,45 @@ const bookStore = {
 
     render : function (){
         const html = this.listBanners.map(banner => {
-            return `<div class="slide" key-id="${banner.id}">
+            return `<div class="slide">
             <img src="${banner.image}">
             </div>`
         });
         slider.innerHTML = html.join('');
+        const lengthBanner = this.listBanners.length;
         let btn = '<div class="btn active"></div>';
-        for (let i = 1 ; i < this.listBanners.length ; i++){
+        for (let i = 1 ; i < lengthBanner ; i++){
             btn += '<div class="btn"></div>'
         }
         btnSlide.innerHTML = btn;
-        const info = this.listBanners.map(info => {
-            return `<div class="title" key-id="${info.id}">${info.title}</div>
-            <div class="description" key-id="${info.id}">${info.description}</div>`
-        })
-        infoSlide.innerHTML = info.join('');
+        const infos = this.listBanners.map(banner => {
+            return {
+                title : banner.title,
+                description :  banner.description
+            };
+        });
+        let info = `<div class="info active">
+                <div class="title">${infos[0].title}</div>
+                <div class="description">${infos[0].description}</div>
+                </div>`;
+        for (let i = 1 ; i < lengthBanner ; i++) {
+            info += `<div class="info">
+                <div class="title">${infos[i].title}</div>
+                <div class="description">${infos[i].description}</div>
+                </div>`;
+        }
+        infoSlide.innerHTML = info;
     },
 
     loadSlide : function(){
         const _This = this;
         const slides = document.querySelectorAll('.slide');
         const btnSlide = document.querySelectorAll('.btn-slide .btn');
-        const titles = document.querySelectorAll('.title');
-        const descriptions = document.querySelectorAll('.description');
+        const info = document.querySelectorAll('.info');
 
         btnSlide.forEach((btn , index) => {
-            btn.onclick = function() {
+            btn.onclick = function() {               
+                info[_This.currentIndex].classList.remove('active')
                 _This.currentIndex = index;
                 slides[0].style.marginLeft = _This.marginLeftSlide + '%';
                 if (_This.marginLeftSlide == 0){
@@ -77,9 +86,11 @@ const bookStore = {
                 document.querySelector('.btn.active').classList.remove('active');
                 btnSlide[_This.currentIndex].classList.add('active');
                 slides[0].style.marginLeft = -_This.marginLeftSlide + '%';
+                info[_This.currentIndex].classList.add('active');
             }
         })
         setInterval(() => {
+            info[_This.currentIndex].classList.remove('active')
             _This.currentIndex++;
             if ( _This.currentIndex > slides.length - 1) {
                 _This.currentIndex = 0;
@@ -92,6 +103,7 @@ const bookStore = {
                 _This.marginLeftSlide = 0;
             }
             slides[0].style.marginLeft = -_This.marginLeftSlide + '%';
+            info[_This.currentIndex].classList.add('active');
         }, 5000)
     },
 
